5c1c2ba8006897ed5e5f16500d69e78a8ebc948f
TJ DeVries <timothydvrs1234@gmail.com>
2017-02-08 06:34:12 -0800

    Add more config options

--- rplugin/python3/descriptive_maps/config.py
+++ rplugin/python3/descriptive_maps/config.py
@@ -1,27 +1,95 @@
+import enum
 import sys
 
-KEY_PREFIX = 'descriptor#'
-LEN_KEY_PREFI = len(KEY_PREFIX)
+KEY_PREFIX = 'descriptive_maps#'
+LEN_KEY_PREFIX = len(KEY_PREFIX)
+
+
+class FilterCase(enum.Enum):
+    ignorecase = 0
+    default = 1
+
 
 DEFAULTS = {
-    'immediate_result': True,
+    'filter_case': {
+        'type': FilterCase,
+        'value': FilterCase.default
+    },
+    'force_refresh': {
+        'type': bool,
+        'value': False
+    },
+    'immediate_result': {
+        'type': bool,
+        'value': True
+    },
+    'max_display_lhs': {
+        'type': int,
+        'value': 25,
+    },
+    'max_display_rhs': {
+        'type': int,
+        'value': 40,
+    },
 }
+
 
 class DescriptorConfig:
     def __init__(self, config_dict: dict):
         for key, value in config_dict.items():
-            self.set('_' + key, value)
+            self.set(key, value)
 
     def set(self, key, value):
         key = key[LEN_KEY_PREFIX:]
-        setattr(self, '_' + str(key), value)
 
-    def _default(self, name):
+        if isinstance(DEFAULTS[key]['type'], enum.EnumMeta):
+            result = getattr(DEFAULTS[key]['type'], value, None)
+
+            # If we get a bad option, just set it to the default value
+            if result is None:
+                result = DEFAULTS[key]['value']
+        else:
+            result = value
+
+        setattr(self, '_' + str(key), result)
+
+    def _default(self):
+        name = sys._getframe(1).f_code.co_name
+
         if not hasattr(self, '_' + name):
-            setattr(self, '_' + name, DEFAULTS[name])
+            setattr(self, '_' + name, DEFAULTS[name]['value'])
+
+        return getattr(self, '_' + name)
+
+    @property
+    def filter_case(self):
+        return self._default()
 
     @property
     def immediate_result(self):
-        self._default(sys._getframe().f_code.co_name)
+        return self._default()
 
-        return self._immediate_result
+    @property
+    def force_refresh(self):
+        return self._default()
+
+    @property
+    def max_display_lhs(self):
+        return self._default()
+
+    @property
+    def max_display_rhs(self):
+        return self._default()
+
+    def to_list(self):
+        props = [
+            name for name, value in vars(DescriptorConfig).items()
+            if isinstance(value, property)
+        ]
+
+        props = sorted(props)
+        prop_string = '{0:20} | {1}'
+        return [
+            prop_string.format(prop, getattr(self, prop))
+            for prop in props
+        ]
